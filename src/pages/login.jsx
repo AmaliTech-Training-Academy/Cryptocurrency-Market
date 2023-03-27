006609fc1ac6305a652deaa36f2a1c6cb77cf207
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-27 11:54:39 +0000

    feature: signup page layout created

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import icon from '../assets/CryptoMart.svg'
 const login = () => {
   return (
-    <div>
-      
+    <div className='w-full flex justify-center items-center '>
+      <div className="">
+        <img src={icon} alt="" />
+        <div className="shadow-inner w-[626px] flex flex-col justify-center items-center pt-[48px] pb-[55px] text-[#101828]">
+          <h2 className='text-[34px] font-normal mb-[25px]'>Create Your Account </h2>
+          <p className='mb-[25px] text-[21.52px]'>Get Started For Free By Signing Up Now.</p>
+          <h1>SIGN UP</h1>
+        </div>
+      </div>
     </div>
   )
 }
