b1d1d8f4d2c0d248c241a8f5cd693bff9a0f8bf5
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-03-28 16:06:12 +0000

    feature: created crypt feature component

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Instagram from "../assets/ri_instagram-fill.svg"
+import Facebook from "../assets/mdi_facebook.svg"
+import Twitter from "../assets/ion_logo-twitter (1).svg"
+import LinkedIn from "../assets/uil_linkedin.svg"
 
 const Footer = () => {
   return ( 
@@ -11,7 +15,7 @@ const Footer = () => {
         <div className='w-[743.68px] h-[190.3px] absolute top-[109px] right-[45.50px] flex justify-between p-[11.29px]'>
           <div className='flex flex-col'>
             <span className='text-[24.51px] text-white/100'>Contact</span>
-            <ul className='flex flex-col gap[11.29px] text-[24.51px] text-[#ffffff9e]'>
+            <ul className='w-[173.58px] h-[142.73px] flex flex-col justify-between text-[24.51px] text-[#ffffff9e] mt-[11.29px]'>
               <li>Email us</li>
               <li>Company</li>
               <li>How it works</li>
@@ -19,7 +23,7 @@ const Footer = () => {
           </div>
           <div className='flex flex-col ml-[-72.24px]'>
           <span className='text-[24.51px] text-white/100'>Support</span>
-            <ul className='flex flex-col gap[11.29px] text-[24.51px] text-[#ffffff9e]'>
+            <ul className='w-[173.58px] h-[142.73px] flex flex-col justify-between text-[24.51px] text-[#ffffff9e] mt-[11.29px]'>
               <li>FAQ's</li>
               <li>Help center</li>
               <li>Consult</li>
@@ -27,14 +31,17 @@ const Footer = () => {
           </div>
           <div className='flex flex-col ml-[-72.24px]'>
           <span className='text-[24.51px] text-white/100'>Get in touch</span>
-            <ul className='flex flex-col text-[24.51px] text-[#ffffff9e] pt-[22.58px] '>
+            <ul className='h-[89.51px] flex flex-col justify-between text-[24.51px] text-[#ffffff9e] mt-[22.58px]'>
               <li>hello@amalitech.com</li>
               <li> +2334567876</li>
             </ul>
           </div>
         </div>
-        <div className='w-[249.99px] h-[35.83px] bg-red-800 absolute top-[292.58px] left-[89.75px] flex justify-between'>
-
+        <div className='w-[249.99px] h-[35.83px] absolute top-[292.58px] left-[89.75px] flex '>
+          <img src={Instagram} alt="" />
+          <img src={Facebook} alt="" className='ml-[36.64px] mr-[33.27px]' />
+          <img src={Twitter} alt="" />
+          <img src={LinkedIn} alt="" className='ml-[33.37px]'/>
         </div>
       </div>
       <div className='h-[67px] bg-[#0c3c4cce] flex justify-center items-center text-white text-base font-normal'>
