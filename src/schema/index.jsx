ff55b1b28d1764c60bac777e7534116a8a667a0b
Harr-iet <harrietbuadee@gmail.com>
2023-03-29 12:49:42 +0000

    feature: validation for user profile page done

--- src/schema/index.jsx
+++ src/schema/index.jsx
@@ -1,9 +1,15 @@
 import * as yup from "yup"
 
 export const basicSchema = yup.object().shape({
-    firstName: yup.string().email("Enter first name").required("Required"),
-    lastName: yup.string().email("Enter first name").required("Required"),
-    email: yup.string().email("Enter email").required("Required"),
-
-
+    image: yup.mixed().required('This field is required').test('fileFormat', 'Unsupported file format', (value) => {
+        if (value) {
+          return ['image/png', 'image/jpeg', 'image/jpg'].includes(value.type);
+        }
+        return true;
+      }),
+    firstName: yup.string().required("Please enter first name"),
+    lastName: yup.string().required("Please enter last name"),
+    email: yup.string().email().required("Please enter email"),
+    mobileNumber: yup.number().required("Please enter mobile number"),
+    gender: yup.string().required("Please select your gender")
 })
