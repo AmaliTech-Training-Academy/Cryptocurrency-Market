5f2a7960c5718c95fc2c0800c0bcdd45bba446c2
Harr-iet <harrietbuadee@gmail.com>
2023-04-03 16:05:38 +0000

    feature: styles for viewcrypto done

--- src/component/Viewarea.jsx
+++ src/component/Viewarea.jsx
@@ -4,18 +4,18 @@ import bnb from '../assets/BNB.png'
 const Viewarea = () => {
   return (
     <div className=''>
-        <div className='w-[1208px] h-[433px] bg-[#e4e7ec] shadow ml-[130px] mb-[50px] '>
+        <div className='w-[1208px] h-[433px] bg-[#e4e7ec] shadow ml-[130px] mb-[50px] rounded '>
             <div className='ml-4'>
-                <div className='flex justify-between mr-[83px]'>
+                <div className='flex mr-[83px]'>
                    <div className='flex mt-4'>
-                   <div><img src={bnb} alt="" /></div>
-                    <div>BNB</div>
+                   <div className='mr-2'><img src={bnb} alt="" /></div>
+                    <div >BNB</div>
                    </div>
-                    <div className='mt-4'>BNB</div>
-                    <div className='mt-4'>$41,650,00</div>
-                    <div className='mt-4 text-[#32D583]'>+45.74%</div>
-                    <div className='mt-4'>$715,650M</div>
-                    <div className='mt-4'>$715,650M</div>
+                    <div className='mt-4 ml-[150px]'>BNB</div>
+                    <div className='mt-4 ml-[132px]'>$41,650,00</div>
+                    <div className='mt-4 text-[#32D583] ml-[82px]'>+45.74%</div>
+                    <div className='mt-4 ml-[162px]'>$715,650M</div>
+                    <div className='mt-4 ml-[149px]'>$715,650M</div>
                 </div>
 
             </div>
