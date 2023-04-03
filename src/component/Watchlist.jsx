8e5af97fa0b784fc02e057f958c114067566407f
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-03 11:08:21 +0000

    feature: Watchlist component created

--- src/component/Watchlist.jsx
+++ src/component/Watchlist.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import bnb from "../assets/BNB.png";
+import terra from "../assets/Terra logo.png";
+import et from "../assets/Et.png";
+import btc from "../assets/btc.png";
 
 const Watchlist = () => {
   return (
-    <div>Watchlist</div>
-  )
-}
+    <div className="p-[40px] pt-[10px]">
+      <h1 className="text-[25px] mb-[24px]">Trending Market</h1>
+      <div className="flex justify-between items-baseline mb-[32px]">
+        <div className="w-[70px] flex justify-between">
+          <img src={bnb} alt="" />
+          <h1>BNB</h1>
+        </div>
+        <h1>$41,650,00</h1>
+      </div>
+      <div className="flex justify-between items-baseline mb-[32px]">
+        <div className="w-[90px] flex justify-between">
+          <img src={btc} alt="" />
+          <h1>Bitcoin</h1>
+        </div>
+        <h1>$41,650,00</h1>
+      </div>
+      <div className="flex justify-between items-baseline mb-[32px]">
+        <div className="w-[110px] flex justify-between">
+          <div className="h-[22px] w-[22px] rounded-full  flex items-center justify-center">
+          <img src={et} alt="" />
+          </div>
+          <h1>Ethereum</h1>
+        </div>
+        <h1>$41,650,00</h1>
+      </div>
+      <div className="flex justify-between items-baseline mb-[32px]">
+        <div className="w-[80px] flex justify-between">
+          <img src={terra} alt="" />
+          <h1>Terra</h1>
+        </div>
+        <h1>$41,650,00</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Watchlist
+export default Watchlist;
