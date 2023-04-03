7a701ac34247bdf89a199daee200a77bca040b99
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-04-03 14:29:03 +0000

    feature: created asset modal

--- src/component/Cryptomodal.jsx
+++ src/component/Cryptomodal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Cryptomodal() {
   return (
-    <div className='w-[433px] h-[366px] bg-white rounded-[15px] relative'>
+    <div className='w-[433px] h-[366px] bg-white rounded-[15px] relative border-2'>
       <span className='w-[86px] h-[86px] bg-[#6CE9A6] rounded-full flex justify-center items-center absolute top-[33px] right-[183px]'>
         <svg width="33" height="25" viewBox="0 0 33 25" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M10.5 19.7761L2.625 11.9403L0 14.5522L10.5 25L33 2.61194L30.375 0L10.5 19.7761Z" fill="white"/>
