2a032bb64c8a013b64e9288f05da257e31db9625
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-04-04 13:58:39 +0000

    feature: created component to display active crypto

---
+++ src/component/Filter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Filter() {
+  return (
+    <div className='w-[301px] h-[329px] bg-red-300 pl-[27px] pt-[36px]'>
+      <div>
+        <input type="checkbox" className='appearance-none w-[22px] h-[22px] border-[1px] border-[#32D583]'/>
+        <span className='font-normal text-[18.8799px] leading-7 ml-7'>Name</span>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
