736edb79cbe6ec8b5e97982658c3491454776b71
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-04-05 09:34:51 +0000

    feature: fixed conflicts and rebased from dev

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -8,6 +8,7 @@ import Statistics from "./Statistics";
 import Popular from "./Popular";
 import Trending from "./Trending";
 
+<<<<<<< HEAD
 export {
   Navbar,
   NaviBar,
@@ -18,4 +19,17 @@ export {
   Statistics,
   Popular,
   Trending,
+=======
+import Cryptomodal from "./Cryptomodal";
+import Assetmodal from "./Assetmodal";
+
+export {
+  Navbar,
+  Footer,
+  Input,
+  Cryptomodal,
+  Assetmodal,
+  NaviBar,
+  Select,
+>>>>>>> cc6c6ed (feature: created component to display active crypto)
 };
