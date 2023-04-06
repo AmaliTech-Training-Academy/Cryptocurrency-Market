8a64d2322f410015576fb5335b9881958fccb6af
Harr-iet <harrietbuadee@gmail.com>
2023-04-06 07:33:25 +0000

    feature:style adjustment made to the page(some styles have been removed

--- src/pages/Viewcrypto.jsx
+++ src/pages/Viewcrypto.jsx
@@ -12,25 +12,6 @@ const Viewcrypto = () => {
                     <button className='w-[87px] h-[38px] bg-[#0C3C4C] text-white text-[16px] rounded-lg'>Edit</button>
                 </div>
             </div>
-
-
-
-            <div className='ml-[130px] mt-[28px]  flex '>
-                <div>
-                    <div className='text-[#515253] mr-[89px]'>Total Balance</div>
-                    <div className='text-[#101828] ml-[20px] mt-[23px]'>$0</div>
-                </div>
-
-                <div>
-                    <div className='text-[#515253] mr-[89px]'>Available</div>
-                    <div className='text-[#101828] mt-[23px] ml-[20px]'>$0</div>
-                </div>
-                <div>
-                    <div className='text-[#515253]'>Open orders</div>
-                    <div className='text-[#101828] mt-[23px] ml-[20px] '>-</div>
-                </div>
-            </div>
-    
         
             <div className=' w-[1208px] h-[45px] pt-[11.8px] ml-[130px] border-b-[0.235999px] border-b-[#C1C1C1] mt-[39px] '>
                     <ul className='flex justify-between font-normal text-[18.8799px] text-[#101828] leading-[23px] pr-[86px] pl-[34.8px]'>
