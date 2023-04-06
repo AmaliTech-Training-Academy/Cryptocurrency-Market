57d61868e04441fef9978e8444f340ff60213358
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-06 08:06:27 +0000

    feature: Statistical graph updates with realtime changes

--- src/component/Statistics.jsx
+++ src/component/Statistics.jsx
@@ -1,24 +1,29 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import AreaChartComponent from "./AreaCharrt";
+import { useSelector } from "react-redux";
 
 const Statistics = () => {
 
+ 
+
     const [data,setData] = useState()
-    let spark
+    const [rate,setRate] = useState()
   useEffect(() => {
     const fetchdata = async () => {
       const response = await axios.get(
         "https://api.coinranking.com/v2/coins?limit=10"
         );
-        const data =  response.data.data.coins[0].sparkline
+        const data =  response.data.data.coins[0].sparkline.slice(0,12)
+        setRate(response.data.data.coins[0].change)
         setData(data.map((items, index)=>{
-            return {hr: index, price: parseFloat(items)}
+            return {hr: index * 2  , price: parseFloat(items)}
           }))
     };
     fetchdata()
-  },[]);
+  },[data]);
   
+
   return ( 
     <div className="pl-[36px] pr-[51px] pt-[13px] pb-[10px] mb-[69px] shadow">
       <h1 className="text-[25px] mb-[10px]">Statistics</h1>
@@ -29,15 +34,14 @@ const Statistics = () => {
         </div>
         <div className="flex  w-[170px] justify-between items-center">
           <h2 className="text-[21px]">US$155.00</h2>
-          <p className="text-[red]">-0.85</p>
+          <p className="text-[red]">{rate}</p>
         </div>
       </div>
       <div className="border-b mb-10"></div>
-      <AreaChartComponent data={data} spark={spark}/>
+      <AreaChartComponent data={data} />
     </div>
   );
 };
 
 export default Statistics;
 
-// https://api.coincap.io/v2/assets/?limit=20
