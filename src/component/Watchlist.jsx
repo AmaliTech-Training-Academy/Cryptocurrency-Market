08737f0056d968a2e2a43f5622d963f693d65110
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-11 09:12:09 +0000

    feature: watchlist height designed

--- src/component/Watchlist.jsx
+++ src/component/Watchlist.jsx
@@ -3,29 +3,28 @@ import bnb from "../assets/BNB.png";
 import terra from "../assets/Terra logo.png";
 import et from "../assets/Et.png";
 import btc from "../assets/btc.png";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 const Watchlist = () => {
-  const {data} = useSelector((store)=>store.watchList)
+  const { data } = useSelector((store) => store.watchList);
   console.log(data);
 
-  return (<>
-       <div className="p-[40px] pt-[10px] mb-[42px] shadow">
-      <h1 className="text-[25px] mb-[24px]">Watchlist</h1>
-   { data.map((item)=>{
-     return(
-      <div className="flex justify-between items-baseline mb-[32px]">
-        <div className="w-[70px] flex justify-between">
-          <img src={btc} alt="" />
-          <h1>{item.name}</h1>
-        </div>
-        <h1>${item.price}</h1>
-    
-    </div>
-    )
-  })
-}
-  </div>
-</>
+  return (
+    <>
+      <div className="p-[40px] pt-[10px] mb-[42px]  shadow overflow-y-auto h-[560px]">
+        <h1 className="text-[25px] mb-[24px]">Watchlist</h1>
+        {data.map((item) => {
+          return (
+            <div className="flex justify-between items-baseline mb-[32px]">
+              <div className="w-[30%]   flex justify-between items-center">
+                <img src={item.iconUrl} alt="" className="w-9" />
+                <h1 className="text-right">{item.name}</h1>
+              </div>
+              <h1>${item.price}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
