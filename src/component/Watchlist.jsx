030d8b4e6bb587a130daf4051a5cc936efa5d12c
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-13 11:26:30 +0000

    feature: dashboard watchlist linked to watchlist page

--- src/component/Watchlist.jsx
+++ src/component/Watchlist.jsx
@@ -4,6 +4,7 @@ import terra from "../assets/Terra logo.png";
 import et from "../assets/Et.png";
 import btc from "../assets/btc.png";
 import { useSelector } from "react-redux";
+import {Link} from "react-router-dom"
 const Watchlist = () => {
   const { data } = useSelector((store) => store.watchList);
 
@@ -11,15 +12,17 @@ const Watchlist = () => {
     <>
       <div className="p-[40px] pt-[10px] mb-[42px]  shadow overflow-y-auto h-[560px] ">
         <h1 className="text-[25px] mb-[24px]">Watchlist</h1>
-        {data.map((item) => {
+        {data.map((item,index) => {
           return (
-            <div className="flex justify-between items-center mb-[32px]">
+            <Link key={index} to={'/watchlist'}>
+            <div  className="flex justify-between items-center mb-[32px]">
               <div className="w-[30%]   flex justify-between items-center">
                 <img src={item.iconUrl} alt="" className="w-9" />
                 <h1 className="text-right">{item.name}</h1>
               </div>
               <h1>${item.price}</h1>
             </div>
+            </Link>
           );
         })}
       </div>
